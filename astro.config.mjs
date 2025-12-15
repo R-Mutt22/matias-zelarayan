df2ed76051f809072667f9d59358ed5413ea78ea
Matías Zelarayán <matiaszelarayandev@gmail.com>
2025-12-15 18:09:49 -0300

    Deploy to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	output: 'static',
+	site: 'https://R-Mutt22.github.io',
+	base: '/portfolio-minimalist/',
+});
